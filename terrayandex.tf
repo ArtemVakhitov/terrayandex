283fd07e6030c787d648af49e5629e4701fa180c
Artem Vakhitov <artem.vakhitov@gmail.com>
2023-10-30 15:54:50 +0600

    Add yandex_compute_instance block

--- terrayandex.tf
+++ terrayandex.tf
@@ -25,3 +25,40 @@ variable "instances" {
   type = number
   default = 1
 }
+
+resource "yandex_compute_instance" "terra" {
+
+  count = var.instances
+
+  name = "terra.${count.index}"
+
+  zone = "ru-central1-b"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = var.cpu
+    memory = var.ram
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8q5m87s3v0hmp06i5c"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2lgv5mqm56n8fjkt37q"
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+
+  connection {
+    host = self.network_interface.0.nat_ip_address
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/devops-eng-yandex-kp.pem")
+  }
+}
